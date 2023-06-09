9d494410d618b19dd6f4cfc9efa965d733315655
Igor Carvalho <igorsilvabhz6@gmail.com>
2023-06-09 13:03:55 -0300

    Add User and Task tables their models and serializers

---
+++ README.md
@@ -0,0 +1,14 @@
+# Hello! This is just a studying repositorie
+
+This is just a initial project
+
+# Details about the project
+
+<details>
+  <summary><strong>Used tecnologies</strong></summary><br />
+
+  - Python
+  - Django
+  - DataBase
+
+</details>
--- todoList/models.py
+++ todoList/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+
+class Task(models.Model):
+    title = models.CharField(max_length=150)
+    status = models.BooleanField(default=True)
+    description = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default="")
+
